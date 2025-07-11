68b3e6232d279dbae5e764f0b8973c7682c3f0db
Francisco Ignácio Fontoura Solla <francisco.solla@gmail.com>
2025-07-10 23:20:25 -0300

    add cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,28 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (
+        process.env.NODE_ENV === "development" ||
+        // Allow requests with no origin (like mobile apps, curl, or same-origin requests)
+        !origin ||
+        allowedOrigins.includes(origin)
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 app.get("/ping", (_, res) => {
   res.json({ pong: true });
